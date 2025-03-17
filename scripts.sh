ec0d6ad41a75804dbbe696e59bff9ab04dbf60ee
dtalari666 <dineshtalari06@gmail.com>
2025-03-17 16:39:01 +0000

    shell script created

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # this is a comment!
 echo web deployment script
+echo "updating system"
+sudo apt update -y
+echo "install zip & unzip"
+sudo apt intall -y zip unzip
+echo "install NGINX"
+sudo install -y nginx
+echo "remove sample pages"
+sudo rm -r /var/www/html/
+echo "clone repository"
+echo "webapp depolyment"
+
